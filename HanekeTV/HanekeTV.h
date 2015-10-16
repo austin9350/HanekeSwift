5cab61d8ea217d4ee02366fc3d4b6b7d91fd1bed
Austin Younts <Austin.R.Younts@partner.bmwgroup.com>
2015-10-16 08:08:48 -0400

    Added a tvOS target.

---
+++ HanekeTV/HanekeTV.h
@@ -0,0 +1,19 @@
+//
+//  HanekeTV.h
+//  HanekeTV
+//
+//  Created by Austin Younts on 10/16/15.
+//  Copyright © 2015 Haneke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HanekeTV.
+FOUNDATION_EXPORT double HanekeTVVersionNumber;
+
+//! Project version string for HanekeTV.
+FOUNDATION_EXPORT const unsigned char HanekeTVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HanekeTV/PublicHeader.h>
+
+
